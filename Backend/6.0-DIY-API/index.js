7ad6f1d5dbba4d1ead75a18bfd6222bdb0080a65
vmagasen <vmagasen@gmail.com>
2025-04-03 00:37:17 +0800

    DIY API

--- Backend/6.0-DIY-API/index.js
+++ Backend/6.0-DIY-API/index.js
@@ -22,15 +22,81 @@ app.get("/jokes/:id",(req, res) => {
 
 //3. GET a jokes by filtering on the joke type
 
+app.get("/filter", (req, res) => {
+  const type = req.query.type;
+  const filteredActivities = jokes.filter((joke) => joke.jokeType === type);
+  res.json(filteredActivities);
+});
+
 //4. POST a new joke
-
+app.post("/jokes", (req, res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeType: req.body.type,
+  };
+  jokes.push(newJoke);
+  console.log(jokes.slice(-1));
+  res.json(newJoke);
+});
 //5. PUT a joke
+app.put("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
 
+  jokes[searchIndex] = replacementJoke;
+  res.json(replacementJoke);
+});
 //6. PATCH a joke
+
+app.patch("/jokes/:id", (req, res) => {
+  const id =parseInt(req.params.id);
+  const existingJoke = jokes.find((joke) => joke.id = id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText, //checks if req has text, if null, use the existingJoke
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+  const searchIndex = jokes.findIndex((joke) => joke.id == id);
+  jokes[searchIndex] = replacementJoke;
+  console.log(jokes[searchIndex]);
+  res.json(replacementJoke);
+});
 
 //7. DELETE Specific joke
 
+app.delete("/jokes/:id", (req, res) => {
+  const id =parseInt(req.params.id);
+  const searchIndex = jokes.findIndex((joke) => joke.id == id);
+  if (searchIndex > -1){
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({error: `Joke with id ${id} not found. No jokes deleted.`});
+  }
+});
+
 //8. DELETE All jokes
+
+app.delete("/all", (req, res) => {
+  const userKey = req.query.key;
+  if (userKey == masterKey){
+    jokes = [];
+    res.sendStatus(200);
+  } else {
+    res
+      .status(404)
+      .json({error: `You are not authorized to do that.`});
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
